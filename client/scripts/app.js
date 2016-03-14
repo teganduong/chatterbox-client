3da18aa6fb4a84ce7e6fb1cac235ce20ec069756
teganduong <teganduong@gmail.com>
2016-03-14 16:00:34 -0700

    Complete outline of methods up to events to pass specs

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,58 @@
-// YOUR CODE HERE:
+var app = {
+  server: 'https://api.parse.com/1/classes/messages',
 
+  init: function() {
+
+  },
+
+
+  send: function(message) {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+  fetch: function() {
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      success: function (data) {
+        console.log('Messages: ', data);
+      },
+      error: function (data) {
+        console.error('chatterbox: Failed to return message', data);
+      }
+    });
+  },
+  clearMessages: function() {
+    $('#chats').empty();
+  },
+  addMessage: function(message) {
+    $('#chats').append('<div>' + message + '</div>');
+
+  },
+  addRoom: function(roomName) {
+    $('#roomSelect').append('<option value=' + roomName + '>' + roomName + '</option>');
+  }
+};
+
+
+// var message = {
+//   username: 'teganduong',
+//   text: 'LOLOL',
+//   roomname: 'main'
+// };
+
+
+
+// $.get('https://api.parse.com/1/classes/messages', function(data) {
+//   console.log(data);
+// });
