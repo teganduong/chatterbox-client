92345f72143aeaf564a98d59bbc3b79289756276
teganduong <teganduong@gmail.com>
2016-03-14 20:08:04 -0700

    Implement addFriend function

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,27 +1,3 @@
-//reference: http://stackoverflow.com/questions/6020714/escape-html-using-jquery
-// List of HTML entities for escaping.
-var htmlEscapes = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#x27;',
-  '/': '&#x2F;'
-};
-
-// Regex containing the keys listed immediately above.
-var htmlEscaper = /[&<>"'\/]/g;
-
-// Escape a string for HTML interpolation.
-_.escape = function(string) {
-  return ('' + string).replace(htmlEscaper, function(match) {
-    return htmlEscapes[match];
-  });
-};
-
-$(document).ready(function() {
-
-});
 
 var app = {
   server: 'https://api.parse.com/1/classes/messages',
@@ -54,10 +30,10 @@ var app = {
       url: app.server,
       type: 'GET',
       success: function (data) {
-        console.log("FETCH WORKING");
         _.each(data.results, function(message) {
           app.addMessage(message);
         });
+        $('.username').on('click', app.addFriend);
       },
       error: function (data) {
         console.error('chatterbox: Failed to return message', data);
@@ -70,7 +46,28 @@ var app = {
   },
 
   addMessage: function(message) {
-    $('#chats').append('<div class="username" name="' + _.escape(message.username) + '"><h2>' + _.escape(message.username) + '</h2><p>' + _.escape(message.text) + '</p></div>');
+    //reference: http://stackoverflow.com/questions/6020714/escape-html-using-jquery
+    // List of HTML entities for escaping.
+    var htmlEscapes = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#x27;',
+      '/': '&#x2F;'
+    };
+
+    // Regex containing the keys listed immediately above.
+    var htmlEscaper = /[&<>"'\/]/g;
+
+    // Escape a string for HTML interpolation.
+    var escape = function(string) {
+      return ('' + string).replace(htmlEscaper, function(match) {
+        return htmlEscapes[match];
+      });
+    };
+
+    $('#chats').append('<div class="username" name="' + escape(message.username) + '"><h2>' + escape(message.username) + '</h2><p>' + escape(message.text) + '</p></div>');
   },
 
   addRoom: function(roomName) {
@@ -78,19 +75,31 @@ var app = {
   },
 
   addFriend: function() {
-    $('.username').on('click', function (event) {
-      $('#friends').append('<option value=' + $(this).attr("name") + '></option>');
-    });
+    var friendName = $(this).attr("name");
+    if (!(friendName in app.friends)) {
+      app.friends[friendName] = friendName;
+      $('#friends').append('<option value=' + friendName + '>' + friendName + '</option>');
+    }
   },
-  handleSubmit: function(message) {
-    $('#submitButton').on('click', function() {
-      app.send(message);
-      app.addMessage(message);
-      console.log(message);
-    });
+  handleSubmit: function() {
+    // var message = {
+    //   username: $('username').val(),
+    //   text: $('message').val()
+    // };
+    console.log("test");
   }
 
 };
 
-app.init();
+$(document).ready(function() {
 
+  $("button").on('click', app.clearMessages);
+
+  $('#submitButton').on('click', app.handleSubmit);
+
+
+
+});
+
+
+app.init();
